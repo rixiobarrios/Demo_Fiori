c4e7288f81f437002b3cff838ab014b1b8d7a0cc
Rixio Barrios <rixio.barrios@sap.com>
2023-05-15 20:12:22 +0000

    Updated View1.controller.js

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,5 +1,6 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/base/Log"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
@@ -10,6 +11,13 @@ sap.ui.define([
         return Controller.extend("project1.controller.View1", {
             onInit: function () {
 
+            },
+            onBeforeRendering: function () {
+                window.message = "A random log message";
+                Log.info(window.message);
+            },
+            onAfterRendering: function () {
+                debugger;
             }
         });
     });
